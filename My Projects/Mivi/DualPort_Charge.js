7dd29e187955719002454e56d1b2432909d186f7
sushmitha-ks123 <83211408+sushmitha-ks123@users.noreply.github.com>
2021-06-15 11:17:19 +0530

    15062021
    commit

--- My Projects/Mivi/DualPort_Charge.js
+++ My Projects/Mivi/DualPort_Charge.js
@@ -1,16 +1,63 @@
-document.querySelectorAll('.accordian-button').forEach(input => {
+function myFunction() {
+    var x = document.getElementById("nav-ul");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+// const menu = document.querySelector('.hamburger');
+// const menuLinks = document.querySelector('.nav-li');
 
-    input.addEventListener('click', () => {
+// menu.addEventListener('click', function() {
+//     menu.classList.toggle('is-active');
+//     menu.classList.toggle('active');
+// })
 
-        const Content_accordian = input.nextElementSibling;
+// burger = document.querySelector('.hamburger');
+// navbar = document.querySelector('.nav-logo-container');
+// navList = document.querySelector('.nav-ul');
+// // rightNav = document.querySelector('.rightNav');
 
-        input.classList.toggle('accordian-button--active');
+// burger.addEventListener('click', () => {
+//     // rightNav.classList.toggle('v-class-resp');
+//     navList.classList.toggle('v-class-resp');
+//     navbar.classList.toggle('h-nav-resp');
+// })
 
-        if (input.classList.contains('accordian-button--active')) {
-            // Content_accordian.sty
-            Content_accordian.style.maxHeight = Content_accordian + "px";
-        } else {
-            Content_accordian.style.maxHeight = 0;
-        }
-    });
-})
+// $('ul.nav-ul li').click(function(e){
+//     e.preventDefault();
+//     if($(this).siblings('li'))
+// })
+
+
+
+// const navSlide = () => {
+//     const burger = document.querySelector('.hamburger');
+//     const nav = document.querySelector('.nav-ul');
+//     const navLinks = document.querySelectorAll('.nav-ul li');
+
+//     burger.addEventListener('click', () => {
+//         //debugger;
+//         nav.classList.toggle('nav-active');
+//     });
+// }
+// navSlide();
+
+// var circle = document.querySelector('.material-btn');
+// var link = document.querySelector('.nav-ul').querySelectorAll('li');
+// var ham = document.querySelector('.hamburger');
+// var main = document.querySelector('main');
+// var win = window;
+
+// function openMenu(event) {
+
+//     circle.classList.toggle('active');
+//     ham.classList.toggle('material-close');
+//     //   main.classList.toggle('active');
+//     for (var i = 0; i < link.length; i++) {
+//         link[i].classList.toggle('active');
+//     }
+//     event.preventDefault();
+//     event.stopImmediatePropagation();
+// }
